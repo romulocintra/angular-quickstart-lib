f0256e5c879db60c0e983c3c26fc0ecabb99fee8
RomuloCintra <romulo.cintra.contractor@bbva.com>
2017-07-13 14:09:48 +0200

    start demoBar

--- src/lib/src/component/lib.component.ts
+++ src/lib/src/component/lib.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-lib',
   templateUrl: './lib.component.html',
-  styleUrls: ['./lib.component.css']
+  styleUrls: ['./lib.component.scss']
 })
 export class LibComponent {
   name = 'Angular Library';
--- src/lib/src/module.ts
+++ src/lib/src/module.ts
@@ -3,9 +3,13 @@ import { NgModule } from '@angular/core';
 import { LibComponent } from './component/lib.component';
 import { LibService } from './service/lib.service';
 
+import { MdToolbarModule } from '@angular/material';
+
+
 @NgModule({
   declarations: [LibComponent],
   providers: [LibService],
-  exports: [LibComponent]
+  exports: [LibComponent],
+  imports: [MdToolbarModule]
 })
 export class LibModule { }
